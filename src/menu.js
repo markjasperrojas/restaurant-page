c294c04a79888cbc88002b1b79097319711a209c
Mark Jasper Rojas <markjasperrojas1@gmail.com>
2026-06-19 13:33:01 +0800

    Add tempo content & styles for menu

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,1 @@
-export function createAndRenderMenu() {
-  content.innerHTML = "";
-
-  const div = document.createElement("div");
-  const p = document.createElement("p");
-
-  div.classList.add("menu");
-  p.textContent = "MENU!";
-
-  div.appendChild(p);
-  content.appendChild(div);
-}
+export function createAndRenderMenu() {}
