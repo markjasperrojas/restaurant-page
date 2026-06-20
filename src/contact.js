3e7ba039a3a34d6298f28c819628fa2d3d9ca378
Mark Jasper Rojas <markjasperrojas1@gmail.com>
2026-06-20 11:05:29 +0800

    Add tempo content & styles for contact

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,43 @@
+import telImage from "./assets/tel.png";
+import addressImage from "./assets/address.png";
+import mapImage from "./assets/map.png";
+
 export function createAndRenderContact() {
   content.innerHTML = "";
 
-  const div = document.createElement("div");
-  const p = document.createElement("p");
+  const contact = document.createElement("div");
+  contact.classList.add("contact");
 
-  div.classList.add("menu");
-  p.textContent = "CONTACT!";
+  const contactInfo = document.createElement("div");
+  contactInfo.classList.add("contact-info");
 
-  div.appendChild(p);
-  content.appendChild(div);
+  for (let i = 0; i < 2; i++) {
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    const p = document.createElement("p");
+
+    if (i === 0) {
+      image.src = telImage;
+      image.alt = "Telephone image";
+      p.textContent = "123456789";
+    } else {
+      image.src = addressImage;
+      image.alt = "House image";
+      p.textContent = "Dolores National Highschool";
+    }
+
+    div.appendChild(image);
+    div.appendChild(p);
+    contactInfo.appendChild(div);
+  }
+
+  contact.appendChild(contactInfo);
+
+  const image = document.createElement("img");
+  image.src = mapImage;
+  image.classList.add("map");
+
+  contact.appendChild(image);
+
+  content.appendChild(contact);
 }
